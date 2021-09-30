fd5841f0239bd8b950b0b2806bca5333bd6daf74
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 12:58:06 -0400

    lecture work-in-progress

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AnimalForm from "./components/AnimalForm";
 import "./App.css";
 
 function App() {
+  throw new Error("ERROR IN APP!!!");
   return (
     <div className="App">
       <h1>Add New Animal</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 
 test("renders without errors", () => {
-    
+    render(<App/>);
 });
