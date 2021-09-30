a9bc7a35870036406318d2a4ada48cff942256bb
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 13:21:08 -0400

    lecture work-in-progress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Add New Animal</h1>
+      <h1 data-testid="header">Add New Animal</h1>
       <h1>Add New Animal</h1>
       <h1>Add New Animal</h1>
       <AnimalForm />
