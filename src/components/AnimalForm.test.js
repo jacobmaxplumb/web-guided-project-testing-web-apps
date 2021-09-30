c11e18b772c6a41ce65be234bb738dcf29e2a3d6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 14:09:42 -0400

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -7,49 +7,49 @@ test("renders without errors", () => {
     render(<AnimalForm/>);
 });
 
-test("displays species when submitting all fields", async () => {
-    //Arrange: render our component
-    render(<AnimalForm />);
-    const species = "canine";
+// test("displays species when submitting all fields", async () => {
+//     //Arrange: render our component
+//     render(<AnimalForm />);
+//     const species = "canine";
 
-    //Act: Submit our form
-    //1. Give species input focus.
-    // const speciesInput = screen.getByPlaceholderText("species");
-    const speciesInput = screen.getByLabelText(/species:/i);
+//     //Act: Submit our form
+//     //1. Give species input focus.
+//     // const speciesInput = screen.getByPlaceholderText("species");
+//     const speciesInput = screen.getByLabelText(/species:/i);
     
-    //2. Type in species.
-    userEvent.type(speciesInput, species);
+//     //2. Type in species.
+//     userEvent.type(speciesInput, species);
 
-    //3. Give the age focus
-    const ageInput = screen.getByLabelText(/age:/i);
-    //4. Type in age.
-    userEvent.type(ageInput, "12");
+//     //3. Give the age focus
+//     const ageInput = screen.getByLabelText(/age:/i);
+//     //4. Type in age.
+//     userEvent.type(ageInput, "12");
 
-    //5. Give the notes focus
-    const noteInput = screen.getByLabelText(/notes:/i);
-    //6. Type in notes.
-    userEvent.type(noteInput, "this cutest");
+//     //5. Give the notes focus
+//     const noteInput = screen.getByLabelText(/notes:/i);
+//     //6. Type in notes.
+//     userEvent.type(noteInput, "this cutest");
 
-    //7. click our submit button
-    const button = screen.getByRole("button");
-    userEvent.click(button);
+//     //7. click our submit button
+//     const button = screen.getByRole("button");
+//     userEvent.click(button);
 
-    //Assert: Check to see if species name appears
+//     //Assert: Check to see if species name appears
     
-    // Promise Way
-    // const speciesPromise = screen.findByText(species);
-    // speciesPromise.then((speciesFeedback)=>{
-    //     expect(speciesFeedback).toBeInTheDocument();
-    // });
+//     // Promise Way
+//     // const speciesPromise = screen.findByText(species);
+//     // speciesPromise.then((speciesFeedback)=>{
+//     //     expect(speciesFeedback).toBeInTheDocument();
+//     // });
 
-    // Async Way
-    // const speciesFeedback = await screen.findByText(species);
-    // expect(speciesFeedback).toBeInTheDocument();
+//     // Async Way
+//     // const speciesFeedback = await screen.findByText(species);
+//     // expect(speciesFeedback).toBeInTheDocument();
 
-    // Await Way
-    await waitFor(()=> {
-        const speciesFeedback = screen.queryByText(species);
-        expect(speciesFeedback).toBeInTheDocument();
-    });
+//     // Await Way
+//     await waitFor(()=> {
+//         const speciesFeedback = screen.queryByText(species);
+//         expect(speciesFeedback).toBeInTheDocument();
+//     });
 
-});
+// });
