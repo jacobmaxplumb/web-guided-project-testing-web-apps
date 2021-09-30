1c79f74fe865ee7dd1eeaec899f50c05e7821832
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 13:40:56 -0400

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -10,5 +10,13 @@ test("displays species when submitting all fields", ()=> {
     //Arrange: render our component
     render(<AnimalForm />);
     //Act: Submit our form
+    //1. Give species input focus.
+    //2. Type in species.
+    //3. Give the age focus
+    //4. Type in age.
+    //5. Give the notes focus
+    //6. Type in notes.
+    //7. click our submit button
+
     //Assert: Check to see if species name appears
 });
