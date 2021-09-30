a52373c85e956bc5bc9cb16d297e05782a98d318
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 13:09:44 -0400

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -14,13 +14,13 @@ test("on mount, new animal header renders", () => {
     //Act: find our header element
     
     //queryBy: if an element does not exist, returns a null value
-    // const headerEl = screen.queryByText("Add New Animal");
+    const headerEl = screen.queryByText("Add Old Animal");
 
     //getBy: if an element does not exist, the test fails
-    const headerEl = screen.getByText("Add Old Animal");
+    // const headerEl = screen.getByText("Add Old Animal");
 
     console.log(headerEl);
-    
+
     //Assert: pass the test if our header element exists
     // expect(headerEl).toBeInTheDocument();
 });
