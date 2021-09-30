722fda3f0da0f5318eaca48823b7760f47636629
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 12:55:51 -0400

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,6 @@
 
 
-describe("login form", ()=>{
-    test("Test 1 when tested", () => {
-        console.log("running the test");
-    });
 
-    test("Test 2 when tested", ()=> {
-        console.log("running the test");
-    });
-
-});
-
-describe("popup", ()=> {
-    test("Test 3 when tested", ()=> {
-        console.log("running the test");
-    })
-    
-    test("Test 4 when tested", ()=> {
-        console.log("running the test");
-    })
+test("Test 1 when tested", () => {
+    console.log("running the test");
 });
