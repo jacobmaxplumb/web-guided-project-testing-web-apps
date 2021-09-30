6ea05f04388e7c1183c12c86708f53cb14baa6f8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-30 13:38:40 -0400

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -4,4 +4,11 @@ import AnimalForm from './AnimalForm';
 
 test("renders without errors", () => {
     render(<AnimalForm/>);
-})
+});
+
+test("displays species when submitting all fields", ()=> {
+    //Arrange: render our component
+    render(<AnimalForm />);
+    //Act: Submit our form
+    //Assert: Check to see if species name appears
+});
